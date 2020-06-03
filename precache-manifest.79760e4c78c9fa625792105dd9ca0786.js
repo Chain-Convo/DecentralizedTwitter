297a9abb25e4fff29c9334a45c794778aab599e8
deepcrazy <312.gupta@gmail.com>
2020-06-03 10:27:39 -0400

    Updates

---
+++ precache-manifest.79760e4c78c9fa625792105dd9ca0786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8fe01a6c02b25a3a4f5b2e8eaab567",
+    "url": "/DecentralizedTwitter/index.html"
+  },
+  {
+    "revision": "a2a1affd17789c2dad26",
+    "url": "/DecentralizedTwitter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f80b42c2766cde7e746",
+    "url": "/DecentralizedTwitter/static/js/2.5399742a.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/DecentralizedTwitter/static/js/2.5399742a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a1affd17789c2dad26",
+    "url": "/DecentralizedTwitter/static/js/main.82588951.chunk.js"
+  },
+  {
+    "revision": "a5c7c30877529fbaa83f",
+    "url": "/DecentralizedTwitter/static/js/runtime-main.0898d456.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2f2a5d065fe4cd873ad45425bcb0792.js"
+  "/DecentralizedTwitter/precache-manifest.79760e4c78c9fa625792105dd9ca0786.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DecentralizedTwitter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
