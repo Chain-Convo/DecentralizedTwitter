6303c5f662c657b3c1730696428d46b5539cff5d
deepcrazy <312.gupta@gmail.com>
2020-06-03 02:05:34 -0400

    Updates

---
+++ precache-manifest.ab1186a7ab47f28c8d8f0159f885f1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41ae2602faf0ed3623c308012fe39c1",
+    "url": "/DecentralizedTwitter/index.html"
+  },
+  {
+    "revision": "3e325843e1fc5135a643",
+    "url": "/DecentralizedTwitter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f80b42c2766cde7e746",
+    "url": "/DecentralizedTwitter/static/js/2.5399742a.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/DecentralizedTwitter/static/js/2.5399742a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e325843e1fc5135a643",
+    "url": "/DecentralizedTwitter/static/js/main.a4d77875.chunk.js"
+  },
+  {
+    "revision": "a5c7c30877529fbaa83f",
+    "url": "/DecentralizedTwitter/static/js/runtime-main.0898d456.js"
+  }
+]);
